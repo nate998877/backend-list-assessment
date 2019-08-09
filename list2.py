9911d483abc78223fc7972727afae0337c86c3f4
Nate <nate998877@gmail.com>
2019-08-09 12:46:52 -0400

    completed assessments in list1.py and list2.py

--- list2.py
+++ list2.py
@@ -1,3 +1,4 @@
+import string
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -16,18 +17,38 @@
 # modify the passed in list.
 # Hint: Don't use `set()`
 def remove_adjacent(nums):
-    """Your code goes here.  Edit this docstring."""
-    return
-
+    return [x for (i, x) in enumerate(nums) if ((i == 0) | (x != nums[i-1])) ]
 
 # E. Given two lists sorted in increasing order, create and return a merged
 # list of all the elements in sorted order. You may modify the passed in lists.
 # The solution should work in "linear" time, making a single pass of both lists.
 # Hint: Don't use `sort` or `sorted` -- they are not linear time.
-def linear_merge(list1, list2):
-    """Your code goes here.  Edit this docstring."""
-    return
 
+def linear_merge(list1, list2):
+    new_list = []
+    i = 0
+    n = 0
+    while i < len(list1) or n < len(list2):
+        if(i == len(list1)):
+            new_list.extend(list2[n:])
+            break
+        if(n == len(list2)):
+            new_list.extend(list1[i:])
+            break
+        if(string.ascii_lowercase.index(list1[i][0]) == string.ascii_lowercase.index(list2[n][0])):
+            new_list.append(list1[i])
+            new_list.append(list2[n])
+            i+=1
+            n+=1
+        if(string.ascii_lowercase.index(list1[i][0]) < string.ascii_lowercase.index(list2[n][0])):
+            new_list.append(list1[i])
+            i+=1
+            continue
+        if(string.ascii_lowercase.index(list2[n][0]) < string.ascii_lowercase.index(list1[i][0])):
+            new_list.append(list2[n])
+            n+=1
+            continue
+    return new_list
 
 
 # Simple provided test() function used in main() to print
